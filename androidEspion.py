0cf3917e41f35b627a1ebbb679d66e9400a3bd97
Ak-shay-n <akshayak7788@gmail.com>
2025-07-24 10:37:52 +0530

    [update] code organize

--- androidEspion.py
+++ androidEspion.py
@@ -172,11 +172,11 @@ r"""
     def on_connect(self):
 
         #ACTUAL CONNECTION
-        ip = self.input_entry.get()
-        threading.Thread(target=self.connect, args=(ip,)).start()
+        #ip = self.input_entry.get()
+        #threading.Thread(target=self.connect, args=(ip,)).start()
 
         #TESTING PURPOSE
-        #self.show_function_page()
+        self.show_function_page()
 
     def connect(self, ip):
         self.status_label.configure(text="Connecting...")
